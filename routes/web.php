e1c57df4da60021c1900e5262e8d4e92b0a91682
Hermínio Da Silva André Francisco <eryyoficial@gmail.com>
2024-08-20 19:41:07 +0100

    Tratamento de Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// PRINCIAPAL ROUTA
+Route::get('/eboostify', function () {
+    return view("welcome");
+});
 
-Route::get('/', function () {
-    return view('welcome');
+// GRUPOS DE ROTAS PARA OS USERS
+Route::prefix("eboostify/user")->group(function(){
+
+    Route::get('', function(){
+        return "user";
+    });
+
+    Route::get('/{id}', function(){
+        return "Detalhes do usuário";
+    });
 });
