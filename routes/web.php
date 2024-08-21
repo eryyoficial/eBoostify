b70e09810c2c70e87497eece37447b2047177206
Hermínio Da Silva André Francisco <eryyoficial@gmail.com>
2024-08-21 17:51:51 +0100

    Routas do UserController

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 // PRINCIAPAL ROUTA
-Route::get('/eboostify', function () {
-    return view("welcome");
-});
 
-// GRUPOS DE ROTAS PARA OS USERS
-Route::prefix("eboostify/user")->group(function(){
+/* ============================= ROUTAS DO USUÁRIO =============================*/
+Route::get('eboostify/usuario/{user}', [\App\Http\Controllers\UserController::class, 'show']);
 
-    Route::get('/{user:email}', function(\App\Models\User $user){
-        return $user;
-    });
+// Route::get('/eboostify', function () {
+//     return view("welcome");
+// });
 
-    Route::get('/jkhhj', function(){
-        return "Detalhes do usuário";
-    });
-});
+// // GRUPOS DE ROTAS PARA OS USERS
+// Route::prefix("eboostify/user")->group(function(){
+
+//     Route::get('/{user:email}', function(\App\Models\User $user){
+//         return $user;
+//     });
+
+//     Route::get('/jkhhj', function(){
+//         return "Detalhes do usuário";
+//     });
+// });
 
 
+// Route::get('request', function(\Illuminate\Http\Request $request){
+//     $r = $request->all();
+//     return $r;
+// });
 
-Route::get('request', function(\Illuminate\Http\Request, $request){
-    $r = $request->all();
 
-    return $r;
-});
